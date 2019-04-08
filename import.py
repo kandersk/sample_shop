9d28406f3a5db4c39c217f8ba5331c52c8504a3c
Josh Love <lovejs@uindy.edu>
2019-04-08 11:59:52 -0400

    Added DB

---
+++ import.py
@@ -0,0 +1,11 @@
+import sqlite3, csv, pandas
+
+conn = sqlite3.connect('store.db')
+c = conn.cursor()
+
+df = pandas.read_csv('./store.csv')
+df.to_sql('store', conn, if_exists='append', index=False)
+
+c.execute("select * from store")
+
+print(c.fetchall())
