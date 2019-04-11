57c3fdc639005b3402a788f8d325226dd20ed5a1
[Evan Parduhn] <[parduhne@uindy.edu]>
2019-04-10 21:09:56 -0400

    Finished data.py/started getsearch.py

--- data.py
+++ data.py
@@ -2,29 +2,20 @@ import sqlite3
 from flask import current_app as app
 DATABASE = 'store.db'
 
-def get_db():
+def get_db(DATABASE):
   #conn = sqlite3.connect('students_v1.db')
   conn = sqlite3.connect(DATABASE)
   c = conn.cursor()
   return c
 
 def get_item_info(itemNumber):
-  dict = {}
-  db = get_db()
-  price = db.execute("select price from store where item = '{}'".format(itemNumber))
-  title = db.execute("select title from store where item = '{}'".format(itemNumber))
-  url = db.execute("select url from store where item = '{}'".format(itemNumber))
-  qty = db.execute("select qty from store where item = '{}'".format(itemNumber))
-  desc = db.execute("select desc from store where item = '{}'".format(itemNumber))
-  shp = db.execute("select shp from store where item = '{}'".format(itemNumber))
-  dict['itemNumber'] = itemNumber
-  dict['price'] = price
-  dict['title'] = title
-  dict['url'] = url
-  dict['quantity'] = qty
-  dict['description'] = desc
-  dict['Shipping price'] = shp
-  return dict
+  db = get_db(DATABASE)
+  db.execute("select item,price,title,url,qty,desc,shp from store where item = {}".format(itemNumber))
+  things = db.fetchall()
+  key = ('itemNum','price','title','url','qty','desc','shp')
+  dct = dict(zip(key,things[0]))
+
+  return dct
 
 
 bob = get_item_info(6)
