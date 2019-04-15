dd8354996c5d29474a89389a908a9692ac1c3391
Ante Lucev <luceva@uindy.edu>
2019-04-15 12:47:47 -0400

    added detail stuff

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, Markup, request, redirect, render_template, url_for, g
 
 import sqlite3
 import statistics
+import data
 
 app = Flask(__name__)
 
@@ -23,6 +24,18 @@ def search():
   # formats on template
 
   return render_template("search.html")
+
+@app.route('/description', methods=['GET','POST'])
+def description():
+  dic = data.get_item_info(6)
+  url = dic['url']
+  name = dic['title']
+  desc = dic['desc']
+  #qty = dict['qty']
+  price = dic['price']
+  #shipping = dict['shp']
+
+  return render_template("description.html", url = url, name = name, desc = desc, price = price)
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
