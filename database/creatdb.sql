3deddbce70cbcf05bb53ad645274ccdbaf22e110
[Evan Parduhn] <[parduhne@uindy.edu]>
2019-04-22 12:05:50 -0400

    Working data.py

--- database/creatdb.sql
+++ database/creatdb.sql
@@ -3,10 +3,10 @@
 
 CREATE TABLE IF NOT EXISTS store(
   item int not null primary key,
-  title text not null,
   price  numeric(9,2) not null,
+  title text not null,
   imageurl text,
   qty int not null,
   descrip text,
-  shp numeric(9,2) not null)
+  shp numeric(9,2) not null
 );
--- database/data.py
+++ database/data.py
@@ -10,7 +10,7 @@ def get_db(DATABASE):
 
 def get_item_info(itemNumber):
   db = get_db(DATABASE)
-  db.execute("select item,price,title,url,qty,desc,shp from store where item = {}".format(itemNumber))
+  db.execute("select item,price,title,imageurl,qty,descrip,shp from store where item = {}".format(itemNumber))
   things = db.fetchall()
   key = ('itemNum','price','title','url','qty','desc','shp')
   dct = dict(zip(key,things[0]))
@@ -18,32 +18,17 @@ def get_item_info(itemNumber):
   return dct
 
 def get_matchs(word):
-    db = get_db(DATABASE)
-    db.execute("select item from store where title like '%{}'".format(word))
-    matches = db.fetchall()
     matches_dict = []
+    db = get_db(DATABASE)
+    db.execute("select item from store where instr(title,'{}')".format(word))
+    matches = db.fetchall()
+    print(len(matches))
     for i in matches:
         matches_dict.append(get_item_info(i[0]))
     return matches_dict
 
 
-# db = get_db(DATABASE)
-# item = (1,2,3,4,5,7,8,9)
-# price = (2.99,5.66,798.4,4.5,666,734.6,69.69,12.34)
-# title = ("thing1","thing2","thing3","thing4","thing5","kindle 2","kindle 3","kindle is bad")
-# url = ("thing1","thing2","thing3","thing4","thing5","thing6","thing7","thing8")
-# qty = (1,2,3,4,5,6,7,8)
-# desc = ("thing1","thing2","thing3","thing4","thing5","thing6","thing7","thing8")
-# shp = (1.99,2.99,3.99,4.99,5.99,6.99,7.99,8.99)
-
-
-# for i in range(len(item)):
-#     query = "insert into store (item,price,title,url,qty,desc,shp) VALUES({i1},{i2},'{i3}','{i4}',{i5},'{i6}',{i7});".format(i1 = item[i], i2 = price[i], i3 = title[i], i4 = url[i],i5 = qty[i], i6 = url[i],i7 = shp[i])
-#     db.execute(query)
-
-# for i in range(9):
-#     print(get_item_info(i))
-
-search_string = "kindle 1"
+# foo = get_item_info(6)
+search_string = "Hex"
 foo = get_matchs(search_string)
 print(foo)
